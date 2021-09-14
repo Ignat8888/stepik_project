9def097a346f1aaa64e88bca802a61dc0a424fa8
Ignat8888 <Ignat8888@gmail.com>
2021-09-14 15:53:56 +0300

    added base files for stepik project

---
+++ conftest.py
@@ -0,0 +1,38 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+import time
+
+def pytest_addoption(parser):
+	parser.addoption("--language"    , action="store", default = "en")
+	parser.addoption("--browser_name", action="store", default = "chrome")
+	
+
+@pytest.fixture(scope="function")	
+def browser(request):
+	
+	browser_name = request.config.getoption("browser_name")
+	language     = request.config.getoption("language")
+	
+	if browser_name == "chrome": 
+		options = Options()
+		options.add_experimental_option(
+			'prefs', 
+			{'intl.accept_languages': language}
+		)
+		
+		browser = webdriver.Chrome(options=options) 
+	    
+	     
+	if browser_name == "firefox":
+	
+		fp = webdriver.FirefoxProfile()
+		fp.set_preference("intl.accept_languages", language)
+		
+		browser = webdriver.Firefox(firefox_profile=fp)
+	
+	yield browser
+	
+	time.sleep(2)
+	browser.quit() 
